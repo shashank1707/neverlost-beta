82acb59d9e92ba5c84b39b41d466f61be1272ef3
unknown <oo.ks.pp@gmail.com>
2021-12-16 18:53:10 +0530

    splash added

---
+++ lib/Screens/splash.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:neverlost_beta/Components/constants.dart';
+import 'package:neverlost_beta/Firebase/auth.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  var opacity = 0.0;
+  @override
+  void initState() {
+    redirect();
+    super.initState();
+  }
+
+  void redirect() async {
+
+    await Future.delayed(const Duration(milliseconds: 500)).then((value) {
+      setState(() {
+        opacity = 1.0;
+      });
+    });
+
+    await Future.delayed(const Duration(seconds: 3)).then((value) {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return FutureBuilder(
+          future: AuthMethods().getCurrentUser(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Container();
+            } else {
+              return Container();
+            }
+          },
+        );
+      }));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: AnimatedOpacity(
+        opacity: opacity,
+        duration: const Duration(seconds: 1),
+        child: SizedBox(
+          width: width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/never_lost_icon.png',
+                width: width / 3,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(32.0),
+                child: Text('NeverLost',
+                    style: TextStyle(
+                        fontSize: 40,
+                        color: backgroundColor1,
+                        fontWeight: FontWeight.bold)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:neverlost_beta/Screens/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        child: Text("asd"),
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
     );
   }
 }
