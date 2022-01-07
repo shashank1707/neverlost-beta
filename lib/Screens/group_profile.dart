16ad1f0e3d5468a3c6ce9752d224129f3a3bd606
unknown <oo.ks.pp@gmail.com>
2022-01-08 02:55:11 +0530

    change name not implemented

--- lib/Screens/group_profile.dart
+++ lib/Screens/group_profile.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:neverlost_beta/Components/constants.dart';
 import 'package:neverlost_beta/Components/loading.dart';
 import 'package:neverlost_beta/Firebase/database.dart';
+import 'package:neverlost_beta/Screens/chatroom.dart';
+import 'package:neverlost_beta/Screens/profile.dart';
+import 'package:neverlost_beta/Screens/userprofile.dart';
 
 class GroupProfile extends StatefulWidget {
   final String groupUID, userUID;
@@ -14,17 +17,20 @@ class GroupProfile extends StatefulWidget {
 
 class _GroupProfileState extends State<GroupProfile> {
   String adminId = '';
+  String groupName = '';
   Map adminInfo = {};
   List membersInfo = [];
   String photoURL = '';
   late int membersLength;
   bool isAdmin = false;
   bool isloading = true;
+  TextEditingController nameController = TextEditingController();
   @override
   void initState() {
     getGroupInfo();
     super.initState();
   }
+
 
   getGroupInfo() {
     DatabaseMethods().groupDetails(widget.groupUID).listen((event) async {
@@ -33,6 +39,7 @@ class _GroupProfileState extends State<GroupProfile> {
         setState(() {
           adminId = event.data()!['admin'];
           photoURL = event.data()!['photoURL'];
+          groupName = event.data()!['name'];
         });
         if (adminId == widget.userUID) {
           setState(() {
@@ -61,36 +68,97 @@ class _GroupProfileState extends State<GroupProfile> {
   }
 
   Widget membersList() {
-    return ListView.builder(
-        itemCount: membersInfo.length,
-        itemBuilder: (context, index) {
-          return Padding(
+    var currrentUser = membersInfo
+        .where((element) => element['uid'] == widget.userUID)
+        .toList()[0];
+    var admin =
+        membersInfo.where((element) => element['uid'] == adminId).toList()[0];
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        Visibility(
+          visible: widget.userUID != adminId,
+          child: Padding(
             padding: const EdgeInsets.all(4.0),
             child: ListTile(
               onTap: () {
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //         builder: (context) => ChatRoom(
-                //             currentUser: widget.currentUser,
-                //             friendUser: membersInfo[index])));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Profile(currentUser: currrentUser)));
               },
               leading: InkWell(
                 child: CircleAvatar(
                   radius: 30,
-                  backgroundImage: NetworkImage(membersInfo[index]['photoURL']),
+                  backgroundImage: NetworkImage(currrentUser['photoURL']),
                 ),
               ),
-              title: Text(membersInfo[index]['name'],
+              title: Text(currrentUser['name'],
                   style: TextStyle(fontWeight: FontWeight.bold)),
-              subtitle: Text(membersInfo[index]['status'],
+              subtitle: Text(currrentUser['status'],
                   style: TextStyle(fontWeight: FontWeight.bold)),
-              trailing: adminId == membersInfo[index]['uid']
-                  ? Text('admin')
-                  : Text(''),
+              trailing:
+                  adminId == currrentUser['uid'] ? Text('Admin') : Text(''),
             ),
-          );
-        });
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: ListTile(
+              onTap: () {
+                if(widget.userUID == adminId){
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Profile(currentUser: currrentUser)));
+                }else{
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => UserProfile(currentUser: currrentUser, friendUserUID: adminId)));
+                }
+                
+              },
+              leading: InkWell(
+                child: CircleAvatar(
+                  radius: 30,
+                  backgroundImage: NetworkImage(admin['photoURL']),
+                ),
+              ),
+              title: Text(admin['name'],
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              subtitle: Text(admin['status'],
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              trailing: Text('Admin')),
+        ),
+        ...(membersInfo).where((element) => element['uid'] != widget.userUID && element['uid'] != adminId).map((member){
+          return Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => UserProfile(currentUser: currrentUser, friendUserUID: member['uid'])));
+                  },
+                  leading: InkWell(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(member['photoURL']),
+                    ),
+                  ),
+                  title: Text(member['name'],
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  subtitle: Text(member['status'],
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  trailing: adminId == member['uid']
+                      ? Text('admin')
+                      : Text(''),
+                ),
+              );
+        })
+      ],
+    );
   }
 
   @override
@@ -122,6 +190,7 @@ class _GroupProfileState extends State<GroupProfile> {
               width: width,
               height: height,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Container(
                     width: width,
@@ -148,7 +217,7 @@ class _GroupProfileState extends State<GroupProfile> {
                                 child: SizedBox(
                                   width: width / 1.85,
                                   child: Text(
-                                    'Unofficial A',
+                                    groupName,
                                     softWrap: true,
                                     style: TextStyle(
                                         color: backgroundColor2,
@@ -193,12 +262,12 @@ class _GroupProfileState extends State<GroupProfile> {
                       ],
                     ),
                   ),
-                  Align(
+                  const Align(
                       alignment: Alignment.centerLeft,
                       child: Padding(
-                        padding: const EdgeInsets.all(8.0),
+                        padding: EdgeInsets.all(8.0),
                         child: Text(
-                          'Group Members',
+                          'Members',
                           style: TextStyle(fontSize: 20),
                         ),
                       )),
